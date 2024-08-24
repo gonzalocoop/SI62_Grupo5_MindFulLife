f4a91d68d0b9b72029563d7c09981287f535dd9d
gonzalocoop <gonzalocoop@gmail.com>
2024-08-23 22:11:59 -0500

    Subida de archivo base

---
+++ SI62_G5/src/main/java/com/zentech/si62_g5/Si62G5Application.java
@@ -0,0 +1,13 @@
+package com.zentech.si62_g5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Si62G5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Si62G5Application.class, args);
+    }
+
+}
---
+++ SI62_G5/src/test/java/com/zentech/si62_g5/Si62G5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zentech.si62_g5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Si62G5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
