ba574bea036bdc0182f72dd164b9dcd4388ceab0
FernandoLaupa <scondmail2004@gmail.com>
2024-09-06 01:37:16 -0500

    Creación Crud de Videos Favoritos

---
+++ SI62_G5/src/main/java/com/zentech/si62_g5/repositories/IVideosFavoritosRepository.java
@@ -0,0 +1,7 @@
+package com.zentech.si62_g5.repositories;
+
+import com.zentech.si62_g5.entities.VideosFavoritos;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IVideosFavoritosRepository extends JpaRepository<VideosFavoritos,Integer> {
+}
---
+++ SI62_G5/src/main/java/com/zentech/si62_g5/serviceimplements/VideosFavoritosServiceImplement.java
@@ -0,0 +1,36 @@
+package com.zentech.si62_g5.serviceimplements;
+
+import com.zentech.si62_g5.entities.VideosFavoritos;
+import com.zentech.si62_g5.repositories.IVideosFavoritosRepository;
+import com.zentech.si62_g5.serviceinterfaces.IVideosFavoritosService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class VideosFavoritosServiceImplement implements IVideosFavoritosService {
+
+    @Autowired
+    private IVideosFavoritosRepository fR;
+
+    @Override
+    public void insert(VideosFavoritos vfav) {
+        fR.save(vfav);
+    }
+
+    @Override
+    public List<VideosFavoritos> list() {
+        return fR.findAll();
+    }
+
+    @Override
+    public void delete(int id) {
+        fR.deleteById(id);
+    }
+
+    @Override
+    public void update(VideosFavoritos vfav) {
+        fR.save(vfav);
+    }
+}
