ad22379d8f430d41d8416eb01717723d73980903
Gonter32 <jhonqug15@gmail.com>
2024-09-10 01:07:36 -0500

    BuscarComentariosPorTituloDeSesion

--- SI62_G5/src/main/java/com/zentech/si62_g5/repositories/IComentariosRepository.java
+++ SI62_G5/src/main/java/com/zentech/si62_g5/repositories/IComentariosRepository.java
@@ -1,9 +1,17 @@
 package com.zentech.si62_g5.repositories;
 
+import com.zentech.si62_g5.dtos.ComentariosDTO;
 import com.zentech.si62_g5.entities.Comentarios;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface IComentariosRepository extends JpaRepository<Comentarios,Integer> {
+public interface IComentariosRepository extends JpaRepository<Comentarios, Integer> {
+    @Query("SELECT c FROM Comentarios c JOIN c.ses s WHERE s.titulo = :tituloSesion")
+    public List<Comentarios> buscarComentariosPorTituloSesion(@Param("tituloSesion") String tituloSesion);
+
 }
