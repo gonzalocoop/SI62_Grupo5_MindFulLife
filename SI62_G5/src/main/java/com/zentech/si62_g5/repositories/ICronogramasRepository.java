f3515af544b26e6c4f515321e2d9121828253126
Gonter32 <jhonqug15@gmail.com>
2024-09-10 16:59:53 -0500

    BuscarCronogramasPorUsernameUsuario

--- SI62_G5/src/main/java/com/zentech/si62_g5/repositories/ICronogramasRepository.java
+++ SI62_G5/src/main/java/com/zentech/si62_g5/repositories/ICronogramasRepository.java
@@ -2,9 +2,15 @@ package com.zentech.si62_g5.repositories;
 
 import com.zentech.si62_g5.entities.Cronogramas;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
 @Repository
 public interface ICronogramasRepository extends JpaRepository<Cronogramas,Integer> {
+    @Query("SELECT c FROM Cronogramas c WHERE c.curUsu.usua.username = :username")
+    List<Cronogramas> findByUsername(String username);
 }
