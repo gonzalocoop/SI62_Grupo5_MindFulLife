6beca9699b5e9f792f931defad9100bd488af056
gonzalocoop <gonzalocoop@gmail.com>
2024-11-02 00:01:50 -0500

    CRUD completo de las tablas de Sesiones y Videos. En sesiones se utilizo cards. En ambos se ingresaron las validaciones correspondientes. Se relacionaron usando las FK

--- FrontEnd_SI62_G5/src/app/app.routes.ts
+++ FrontEnd_SI62_G5/src/app/app.routes.ts
@@ -12,6 +12,24 @@ import { CreaeditasuscripcionesComponent } from './components/suscripciones/crea
 import { RolesComponent } from './components/roles/roles.component';
 import { CreaeditarolesComponent } from './components/roles/creaeditaroles/creaeditaroles.component';
 import { VerdescripcionComponent } from './components/cursos/verdescripcion/verdescripcion.component';
+import { SesionesComponent } from './components/sesiones/sesiones.component';
+import { CreaeditasesionesComponent } from './components/sesiones/creaeditasesiones/creaeditasesiones.component';
+import { VideosComponent } from './components/videos/videos.component';
+import { CreaeditavideosComponent } from './components/videos/creaeditavideos/creaeditavideos.component';
+import { VideosfavoritosComponent } from './components/videosfavoritos/videosfavoritos.component';
+import { CreaeditavideosfavoritosComponent } from './components/videosfavoritos/creaeditavideosfavoritos/creaeditavideosfavoritos.component';
+import { UsuariosComponent } from './components/usuarios/usuarios.component';
+import { CreaeditausuariosComponent } from './components/usuarios/creaeditausuarios/creaeditausuarios.component';
+import { CursosusuariosComponent } from './components/cursosusuarios/cursosusuarios.component';
+import { CreaeditacursosusuariosComponent } from './components/cursosusuarios/creaeditacursosusuarios/creaeditacursosusuarios.component';
+import { UsuariossuscripcionesComponent } from './components/usuariossuscripciones/usuariossuscripciones.component';
+import { CreaeditausuariossuscripcionesComponent } from './components/usuariossuscripciones/creaeditausuariossuscripciones/creaeditausuariossuscripciones.component';
+import { CronogramasComponent } from './components/cronogramas/cronogramas.component';
+import { CreaeditacronogramasComponent } from './components/cronogramas/creaeditacronogramas/creaeditacronogramas.component';
+import { ComentariosComponent } from './components/comentarios/comentarios.component';
+import { CreaeditacomentariosComponent } from './components/comentarios/creaeditacomentarios/creaeditacomentarios.component';
+import { VerdescripcionsesComponent } from './components/sesiones/verdescripcionses/verdescripcionses.component';
+import { VercomentarioComponent } from './components/comentarios/vercomentario/vercomentario.component';
 
 export const routes: Routes = [
     {
@@ -69,6 +87,100 @@ export const routes: Routes = [
             },
             {
                 path:'ediciones/:id',component:CreaeditarolesComponent
+            }
+        ]
+    },
+    {
+        path:'sesiones',component:SesionesComponent,
+        children:[
+            {
+                path:'nuevo', component:CreaeditasesionesComponent
+            },
+            {
+                path:'ediciones/:id',component:CreaeditasesionesComponent
+            },
+            {
+                path:'descripciones/:id',component:VerdescripcionsesComponent
+            }
+        ]
+    },
+    {
+        path:'videos',component:VideosComponent,
+        children:[
+            {
+                path:'nuevo', component:CreaeditavideosComponent
+            },
+            {
+                path:'ediciones/:id',component:CreaeditavideosComponent
+            }
+        ]
+    },
+    {
+        path:'videosfav',component:VideosfavoritosComponent,
+        children:[
+            {
+                path:'nuevo', component:CreaeditavideosfavoritosComponent
+            },
+            {
+                path:'ediciones/:id',component:CreaeditavideosfavoritosComponent
+            }
+        ]
+    },
+    {
+        path:'usuarios',component:UsuariosComponent,
+        children:[
+            {
+                path:'nuevo', component:CreaeditausuariosComponent
+            },
+            {
+                path:'ediciones/:id',component:CreaeditausuariosComponent
+            }
+        ]
+    },
+    {
+        path:'cursosusuarios',component:CursosusuariosComponent,
+        children:[
+            {
+                path:'nuevo', component:CreaeditacursosusuariosComponent
+            },
+            {
+                path:'ediciones/:id',component:CreaeditacursosusuariosComponent
+            }
+        ]
+    },
+    {
+        path:'usuariossuscripciones',component:UsuariossuscripcionesComponent,
+        children:[
+            {
+                path:'nuevo', component:CreaeditausuariossuscripcionesComponent
+            },
+            {
+                path:'ediciones/:id',component:CreaeditausuariossuscripcionesComponent
+            }
+        ]
+    },
+    {
+        path:'cronogramas',component:CronogramasComponent,
+        children:[
+            {
+                path:'nuevo', component:CreaeditacronogramasComponent
+            },
+            {
+                path:'ediciones/:id',component:CreaeditacronogramasComponent
+            }
+        ]
+    },
+    {
+        path:'comentarios',component:ComentariosComponent,
+        children:[
+            {
+                path:'nuevo', component:CreaeditacomentariosComponent
+            },
+            {
+                path:'ediciones/:id',component:CreaeditacomentariosComponent
+            },
+            {
+                path:'vercoment/:id',component:VercomentarioComponent
             }
         ]
     },
